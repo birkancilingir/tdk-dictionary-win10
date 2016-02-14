cc68266ebfa2c838e34bc8d67184d0064515cc81
Birkan Çilingir <birkancilingir@hotmail.com>
2016-02-14 23:39:19 +0200

    v1.5.0.0
    Rewriting for windows 10

---
+++ TdkDictionaryWin10/Views/Splash.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-SplashScreen
+namespace TdkDictionaryWin10.Views
+{
+    public sealed partial class Splash : UserControl
+    {
+        public Splash(SplashScreen splashScreen)
+        {
+            InitializeComponent();
+            Window.Current.SizeChanged += (s, e) => Resize(splashScreen);
+            Resize(splashScreen);
+        }
+
+        private void Resize(SplashScreen splashScreen)
+        {
+            if (splashScreen.ImageLocation.Top == 0)
+            {
+                MyImage.Visibility = Visibility.Collapsed;
+                return;
+            }
+            else
+            {
+                MyCanvas.Background = null;
+                MyImage.Visibility = Visibility.Visible;
+            }
+            MyImage.Height = splashScreen.ImageLocation.Height;
+            MyImage.Width = splashScreen.ImageLocation.Width;
+            MyImage.SetValue(Canvas.TopProperty, splashScreen.ImageLocation.Top);
+            MyImage.SetValue(Canvas.LeftProperty, splashScreen.ImageLocation.Left);
+            ProgressTransform.TranslateY = MyImage.Height / 2;
+        }
+    }
+}
