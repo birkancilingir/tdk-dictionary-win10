e11af15731492974175cf93b2086eb37fee6b7d8
Birkan Çilingir <birkancilingir@hotmail.com>
2017-05-18 23:35:50 +0300

    v1.5.0.0

---
+++ TdkDictionaryWin10/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace TdkDictionaryWin10.Converters
+{
+    class BooleanToVisibilityConverter : IValueConverter
+    {
+        // Set to the boolean value that will be converted as visible
+        private bool referenceValue = true;
+        public bool ReferenceValue
+        {
+            get { return referenceValue; }
+            set { referenceValue = value; }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (value is bool && (bool)value == referenceValue) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value is Visibility && (Visibility)value == Visibility.Visible && referenceValue;
+        }
+    }
+}
